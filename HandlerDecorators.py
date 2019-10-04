aa29f5b69610046899516919a668e49161a0947b
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2019-10-03 22:57:02 -0400

    Use wrapt to write decorators

--- HandlerDecorators.py
+++ HandlerDecorators.py
@@ -1,14 +1,15 @@
-from AIConifg import AIConfig
+from AIConfig import AIConfig
 from telethon import events
+from wrapt import decorator
 
 class GroupHandler(object):
-    '''Decorator for wrapping telethon NewMessage handlers as group message handlers
+    ''' Decorator for wrapping async telethon NewMessage handlers as group message handlers
         Making group configuration avaliable inside the handlers
 
         Example Usage:
-        @bot.on(events.NewMessage
+        @bot.on(events.NewMessage(...))
         @GroupHandler(ai_config)
-        def handler(event, gid, config):
+        async def handler(event, gid, config):
             # gid would be the group id associated with the incoming message
             # config would be the group config associated with the group
     '''
@@ -18,15 +19,27 @@ class GroupHandler(object):
             ai_config: AIConfig, the configuration object to pull group config from
         '''
         self._config = ai_config
-    
-    def __call__(self, f):
+
+    @decorator
+    async def __call__(self, f, instance, args, kwargs):
         '''Decorates function `f`'''
-        async def func(event: events.NewMessage):
-            if not event.is_group:
-                return None
-            gid = event.chat_id
-            g_config = self._config.group(gid)
-            if g_config is None:
-                return None
-            return await f(event, gid, g_config)
-        return func
+        # Getting the first argument event to the wrapped call
+        event = kwargs.get("event")
+        if event is None:
+            event = args[0]
+        assert isinstance(event, events.NewMessage), "Incorrect event type"
+
+        # Group handler only handles group messages
+        if not event.is_group:
+            return None
+        gid = event.chat_id
+        g_config = self._config.group(gid)
+        # Group handler only handles messages from configured groups
+        if g_config is None:
+            return None
+        
+        # Proxy call to inner method
+        if instance:
+            return await instance.f(event, gid, g_config)
+        # Proxy call to inner function
+        return await f(event, gid, g_config)
